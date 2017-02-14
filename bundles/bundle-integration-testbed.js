801099b9d4dbf0611a94a1d388ccaf77f3cdb4fa
phiku <philipp.kuederli@zhdk.ch>
2017-02-14 16:34:45 +0100

    fix: show detailed name for institutional groups on permissions form

--- bundles/bundle-integration-testbed.js
+++ bundles/bundle-integration-testbed.js
@@ -190,6 +190,7 @@ AppResource = require('./shared/app-resource.coffee');
 
 module.exports = AppResource.extend({
   type: 'Group',
+  extraProperties: 'allow',
   props: {
     name: ['string']
   }
